b90bd2b47afc5f5d98a9ff8bf5be1332e876b91a
paul_hazy <pavelpv8@gmail.com>
2020-11-06 00:55:15 +0400

    update db

--- db.py
+++ db.py
@@ -1,19 +1,23 @@
 import psycopg2
 from config import db_pass
 
-conn = psycopg2.connect(
-    database = 'hh',
-    user     = 'phz',
-    password = db_pass,
-    host     =  'localhost',
-    port     =   '5432'
-)
 
-print('Connection successfully!')
-cur = conn.cursor()
-cur.execute(
-    "INSERT INTO STUDENT (ADMISSION,NAME,AGE,COURSE,DEPARTMENT) VALUES (3420, 'John', 18, 'Computer Science', 'ICT')"
-)
-conn.commit()
-print("insert success")
-conn.close()
+class db_connection:
+    """ Connection to postgres db  """
+
+    conn = psycopg2.connect(
+        database = 'hh',
+        user     = 'phz',
+        password = db_pass,
+        host     =  'localhost',
+        port     =   '5432'
+    )
+
+    print('Connection successfully!')
+    cur = conn.cursor()
+    cur.execute(
+        "INSERT INTO STUDENT (ADMISSION,NAME,AGE,COURSE,DEPARTMENT) VALUES (3420, 'John', 20, 'Computer Science', 'ICT')"
+    )
+    conn.commit()
+    print("insert success")
+    conn.close()
